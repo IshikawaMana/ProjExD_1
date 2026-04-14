3ce11bf42a46fce523da92bacc014e3c8ebb13b5
c0c250085b <c0c250085b@edu.teu.ac.jp>
2026-04-14 16:13:27 +0900

    練習7と8と9:背景のループ

--- pygame_image.py
+++ pygame_image.py
@@ -9,22 +9,27 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    original1 = pg.image.load("fig/pg_bg.jpg")
+    flipped1 = pg.transform.flip(original1, True, False)
+    
     tmr = 0
 
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False) #左右反転
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0) #10度反時計回転
 
-    bg_rct = bg_img.get_rect()
+    x = 0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        bg_rct.move_ip(-1, 0)
-        screen.blit(bg_img, bg_rct)
+        x = (x + 1) % 3200
 
+        screen.blit(original1, (-x, 0))
+        screen.blit(flipped1, (-x + 1600, 0))
+        screen.blit(original1, (-x + 3200, 0))
+        
         screen.blit(kk_img, [300, 200])
 
         tmr += 1        
