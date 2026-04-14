55f50bf018f9a0f1a574a98244dd74e842a38695
c0c250085b <c0c250085b@edu.teu.ac.jp>
2026-04-14 16:23:50 +0900

    練習10:こうかとんの移動

--- pygame_image.py
+++ pygame_image.py
@@ -9,8 +9,8 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    original1 = pg.image.load("fig/pg_bg.jpg")
-    flipped1 = pg.transform.flip(original1, True, False)
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    flipped1 = pg.transform.flip(bg_img, True, False)
     
     tmr = 0
 
@@ -20,17 +20,30 @@ def main():
 
     x = 0
 
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = (x + 1) % 3200
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1, 0)
 
-        screen.blit(original1, (-x, 0))
+        x = tmr % 3200
+
+        screen.blit(bg_img, (-x, 0))
         screen.blit(flipped1, (-x + 1600, 0))
-        screen.blit(original1, (-x + 3200, 0))
+        screen.blit(bg_img, (-x + 3200, 0))
         
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)
 
         tmr += 1        
         clock.tick(200)
